e403f93b710c0ef0a5d1c0773a5989f52e55f59c
DEEPAK-225 <kumarvermadeepak225@gmail.com>
2024-04-28 18:16:47 +0530

    added more functionality

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 const spinBtn = document.getElementById("spinBtn");
-const element = document.getElementById("Spinner");
+const spinner = document.getElementById("Spinner");
+const elements = document.querySelector(".elements-container");
+const notifyPopUp = document.getElementById("notifyPopUp");
+const playAgainBtn = document.getElementById("playAgainBtn"); 
 
-// Define the animation properties
-let stopPosition = 5400+45*Math.floor(Math.random() * 8);
+let spinnerOutput;
+let gameResult;
+elements.addEventListener("click", (e)=>{
+notifyPopUp.style.zIndex = "-1";
+let playerchoice = e.target.getAttribute("data");
+let random = Math.floor(Math.random() * 8);
+spinnerOutput = random;
+gameResult = spinnerOutput==playerchoice;
+
+});
+
+
+spinBtn.addEventListener("click", ()=>{
+   spinBtn.style.display = "none";
+   document.getElementById("arrow").style.display = "block";
+   document.documentElement.style.setProperty('--offset', "30vh");
+   // Define the animation properties
+   let stopPosition = 5400+45*spinnerOutput;
 const keyframes = [
   { transform: 'rotate(0deg)' },
-  { transform: `rotate(${stopPosition}deg)` }
+  { transform: ` rotate(${stopPosition}deg)` }
 ];
-
 const options = {
-  duration: 5000, // 5 seconds
-  iterations: 1,
-  fill: 'forwards',
-  easing: 'ease-in-out' // Apply ease-in-out timing function
-};
-
-// Create the animation
-const animation = element.animate(keyframes, options);
-
- 
+   duration: 5000, // 5 seconds
+   iterations: 1,
+   fill: 'forwards',
+   easing: 'ease-in-out' // Apply ease-in-out timing function
+ };
+ // Create the animation
+spinner.animate(keyframes, options);
 
 setTimeout(() => {
-   spinBtn.style.display = "block";  
-}, 5100);
-spinBtn.addEventListener("click", ()=>{
-   location.reload();
-   // styleSheet.insertRule(keyframes, 0);
-});
-
-
+   notifyPopUp.style.zIndex = "10";
+   showGameResult(gameResult);
+   playAgainBtn.style.display = "block";
+}, 5000);
 
 });
+
+playAgainBtn.addEventListener("click", ()=>{
+   playAgainBtn.style.display = "none";
+   notifyPopUp.style.zIndex = "10";
+   notifyPopUp.textContent = "Choose A Number";
+   notifyPopUp.style.backgroundColor = "#f35500";
+   document.documentElement.style.setProperty('--offset', "23vh");
+   document.getElementById("arrow").style.display = "none";
+   spinBtn.style.display = "block";
+});
+
+function showGameResult(condition){
+   if(condition){
+      notifyPopUp.textContent = "You Won!";
+      notifyPopUp.style.backgroundColor = "green";
+   }
+   else{
+      notifyPopUp.textContent = "You Lose!";
+      notifyPopUp.style.backgroundColor = "red";
+   }
+}
+
+});
+
