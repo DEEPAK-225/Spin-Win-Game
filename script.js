1f7b5f1b6e51a4a267d4e6cc08520bcac7cbf975
DEEPAK-225 <kumarvermadeepak225@gmail.com>
2024-04-28 22:47:06 +0530

    bugs removed

--- script.js
+++ script.js
@@ -4,22 +4,45 @@ const spinner = document.getElementById("Spinner");
 const elements = document.querySelector(".elements-container");
 const notifyPopUp = document.getElementById("notifyPopUp");
 const playAgainBtn = document.getElementById("playAgainBtn"); 
+const arrow = document.getElementById("arrow");
+const userChoice = document.getElementById("playerChoice");
 
-let spinnerOutput;
-let gameResult;
-elements.addEventListener("click", (e)=>{
-notifyPopUp.style.zIndex = "-1";
-let playerchoice = e.target.getAttribute("data");
+function showGameResult(condition){
+   if(condition == true){
+      notifyPopUp.textContent = "You Won!";
+      notifyPopUp.style.backgroundColor = "green";
+      arrow.classList.add("arrow1");
+   }
+   else{
+      notifyPopUp.textContent = "You Lose!";
+      notifyPopUp.style.backgroundColor = "red";
+   }
+}
+let clickEnabled = true;
+let spinnerOutput,playerchoice,gameResult;
+elements.addEventListener("click", (e) => {
+   // Check if click event should be processed
+   if (!clickEnabled) {
+      return; // Exit the function if click is disabled
+  }
+playerchoice = e.target.getAttribute("data");
+if(playerchoice!=null){
+   notifyPopUp.style.zIndex = "-1";
+   userChoice.textContent = `You Choose ${playerchoice}`;
+   clickEnabled = false;
+}
+console.log(playerchoice);
 let random = Math.floor(Math.random() * 8);
 spinnerOutput = random;
-gameResult = spinnerOutput==playerchoice;
-
+arrow.classList.remove("arrow1");
 });
 
-
 spinBtn.addEventListener("click", ()=>{
+   console.log(spinnerOutput);
+   gameResult = spinnerOutput==playerchoice;
+   // userChoice.textContent = `You Choose ${playerchoice}`;
    spinBtn.style.display = "none";
-   document.getElementById("arrow").style.display = "block";
+   arrow.style.display = "block";
    document.documentElement.style.setProperty('--offset', "30vh");
    // Define the animation properties
    let stopPosition = 5400+45*spinnerOutput;
@@ -37,14 +60,17 @@ const options = {
 spinner.animate(keyframes, options);
 
 setTimeout(() => {
-   notifyPopUp.style.zIndex = "10";
+   // console.log(spinnerOutput);
+   // console.log(playerchoice);
    showGameResult(gameResult);
+   notifyPopUp.style.zIndex = "10";
    playAgainBtn.style.display = "block";
 }, 5000);
 
 });
 
 playAgainBtn.addEventListener("click", ()=>{
+   userChoice.textContent = "";
    playAgainBtn.style.display = "none";
    notifyPopUp.style.zIndex = "10";
    notifyPopUp.textContent = "Choose A Number";
@@ -52,18 +78,9 @@ playAgainBtn.addEventListener("click", ()=>{
    document.documentElement.style.setProperty('--offset', "23vh");
    document.getElementById("arrow").style.display = "none";
    spinBtn.style.display = "block";
+   clickEnabled = true;
 });
 
-function showGameResult(condition){
-   if(condition){
-      notifyPopUp.textContent = "You Won!";
-      notifyPopUp.style.backgroundColor = "green";
-   }
-   else{
-      notifyPopUp.textContent = "You Lose!";
-      notifyPopUp.style.backgroundColor = "red";
-   }
-}
 
 });
 
